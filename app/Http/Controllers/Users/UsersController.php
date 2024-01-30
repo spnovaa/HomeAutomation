c49ac306ba4c6b174915fa48132277f382548d63
KTE\m.rahmati <m.rahmati@kte.ir>
2024-01-30 17:42:23 +0330

    FEATURE: user controller connected to the services.

--- app/Http/Controllers/Users/UsersController.php
+++ app/Http/Controllers/Users/UsersController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Users;
 
 use App\Http\Controllers\Controller;
-use App\Models\Device;
 use App\Models\User;
 use App\Services\User\Service as UserService;
 use App\Traits\Controllers\ResourceControllerTrait;
@@ -19,9 +18,15 @@ class UsersController extends Controller
         $this->service = $service;
     }
 
-    protected function getModelFromRequest(Request $request): Device
+    protected function getModelFromRequest(Request $request): User
     {
-
+        return new User([
+            'U_Id' => $request['id'],
+            'U_Name' => $request['name'],
+            'U_LName' => $request['lastName'],
+            'U_UsrName' => $request['username'],
+            'U_Password' => hash('sha256', $request['password'])
+        ]);
     }
 
     #-------------------------------------------------------------------------------------------------------------------
@@ -29,17 +34,11 @@ class UsersController extends Controller
     // we put the registration and authentication methods here.
     public function register(Request $request)
     {
-        $user = new User([
-            'U_Name' => $request['name'],
-            'U_LName' => $request['lastName'],
-            'U_UsrName' => $request['username'],
-            'U_Password' => hash('sha256', $request['password'])
-        ]);
+        $user = $this->getModelFromRequest($request);
 
         $res = $this->service->create($user);
 
-        if ($res->isSuccesssType())
-        {
+        if ($res->isSuccessType()) {
             Auth::login($user);
             return response()->json([
                 'message' => 'ثبت نام با موفقیت انجام شد',
@@ -53,14 +52,40 @@ class UsersController extends Controller
         ], 406);
     }
 
+    // for now, to prevent middleware implementations for this project(that is not required),
+    // we put the registration and authentication methods simply here. they would be developed soon
     public function Login(Request $request)
     {
+        $username = $request['username'];
+        $password = $request['password'];
+        $user = User::where([
+            'U_Username' => $username,
+            'U_Password' => hash('sha256', $password)
+        ])->first();
 
+        if ($user) {
+            Auth::login($user);
+            return response()->json([
+                'message' => 'ورود با موفقیت انجام شد',
+                'code' => 'success'
+            ], 200);
+        }
+
+        return response()->json([
+            'message' => 'اطلاعات کاربری صحیح نیست',
+            'code' => 'error'
+        ], 406);
     }
 
+    // for now, to prevent middleware implementations for this project(that is not required),
+    // we put the registration and authentication methods simply here. they would be developed soon
     public function Logout(Request $request)
     {
-
+        Auth::logout();
+        return response()->json([
+            'message' => 'خروج با موفقیت انجام شد',
+            'code' => 'success'
+        ], 200);
     }
 
     use ResourceControllerTrait;
