d6502a07ea34facb76cc0f62bbfbba39083ae913
KTE\m.rahmati <m.rahmati@kte.ir>
2024-01-30 14:20:54 +0330

    FEATURE: user services developed.

--- app/Http/Controllers/Users/UsersController.php
+++ app/Http/Controllers/Users/UsersController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Users;
 
 use App\Http\Controllers\Controller;
 use App\Models\Device;
+use App\Models\User;
 use App\Services\User\Service as UserService;
 use App\Traits\Controllers\ResourceControllerTrait;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
@@ -22,19 +24,43 @@ class UsersController extends Controller
 
     }
 
+    #-------------------------------------------------------------------------------------------------------------------
+    // for now, to prevent middleware implementations for this project,
+    // we put the registration and authentication methods here.
     public function register(Request $request)
     {
-        
+        $user = new User([
+            'U_Name' => $request['name'],
+            'U_LName' => $request['lastName'],
+            'U_UsrName' => $request['username'],
+            'U_Password' => hash('sha256', $request['password'])
+        ]);
+
+        $res = $this->service->create($user);
+
+        if ($res->isSuccesssType())
+        {
+            Auth::login($user);
+            return response()->json([
+                'message' => 'ثبت نام با موفقیت انجام شد',
+                'code' => 'success'
+            ], 200);
+        }
+
+        return response()->json([
+            'message' => $res->getMessage(),
+            'code' => 'error'
+        ], 406);
     }
 
     public function Login(Request $request)
     {
-        
+
     }
 
     public function Logout(Request $request)
     {
-        
+
     }
 
     use ResourceControllerTrait;
--- app/Services/User/Service.php
+++ app/Services/User/Service.php
@@ -1,8 +1,56 @@
 <?php
 
 namespace App\Services\User;
+use App\Models\User;
+use App\Services\User\Create\Service as CreateService;
+use App\Services\User\Delete\Service as DeleteService;
+use App\Services\User\Update\Service as UpdateService;
+use App\Services\Log\Service as LogService;
 
 class Service
 {
+    private $create_service;
+    private $delete_service;
+    private $update_service;
+    private $log_service;
+
+
+    public function __construct(CreateService $c_s, DeleteService $d_s, UpdateService $u_s, LogService $l_s)
+    {
+        $this->create_service = $c_s;
+        $this->delete_service = $d_s;
+        $this->update_service = $u_s;
+        $this->log_service = $l_s;
+    }
+
+    public function create(User $user)
+    {
+        $res = $this->create_service->create($user);
+        if ($res->isSuccessType())
+            $this->log_service->saveLog('ایجاد یوزر');
+
+        return $res;
+
+    }
+
+    public function delete(User $user)
+    {
+        $res = $this->delete_service->delete($user);
+        if ($res->isSuccessType())
+            $this->log_service->saveLog('حذف یوزر');
+
+        return $res;
+
+    }
+
+    public function update(User $user)
+    {
+        $res = $this->update_service->delete($user);
+        if ($res->isSuccessType())
+            $this->log_service->saveLog('ویرایش یوزر');
+
+        return $res;
+
+    }
 
 }
